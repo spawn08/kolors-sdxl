060cdf8ac254fe06ed8c6c2a3af1fd53daae3014
amarsingh.thakur <amarsingh.thakur@ril.com>
2025-01-06 18:02:39 +0530

    - Initial git commit for kolors model

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kolors_sdxl",
+    version="0.0.2",
+    author="Kolors",
+    description="The training and inference code for Kolors models.",
+    packages=find_packages(),
+    install_requires=[],
+    dependency_links=[],
+)
